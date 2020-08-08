5b599457154cf139e8110053112ccf4cf7b3a18d
Archie <archie.isdiningrat@gmail.com>
2020-08-08 14:58:44 +0700

    improve spinner

--- src/views/pages/Login.js
+++ src/views/pages/Login.js
@@ -1,64 +1,113 @@
-import React from "react"
-import { Lock, Mail } from "react-feather"
-import { connect } from "react-redux"
-import { Button, Card, CardBody, Form, FormGroup, Input, Label, Row } from "reactstrap"
-import "../../assets/scss/pages/authentication.scss"
+import React from "react";
+import { Lock, Mail } from "react-feather";
+import { connect } from "react-redux";
+import {
+  Button,
+  Card,
+  CardBody,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+  Col,
+  Spinner,
+} from "reactstrap";
+import "../../assets/scss/pages/authentication.scss";
 // import Checkbox from "../../components/@vuexy/checkbox/CheckboxesVuexy"
-import { history } from "../../history"
-import axios from '../../http'
-import { LoginAction } from '../../redux/actions/auth'
+import { history } from "../../history";
+import axios from "../../http";
+import { LoginAction } from "../../redux/actions/auth";
 
 class Login extends React.Component {
   state = {
-    username : "archisdi",
-    password: "archisdi"
-  }
+    username: "archisdi",
+    password: "archisdi",
+    is_loading: false,
+  };
 
   async login() {
-    await axios.post('/auth/login', { username: this.state.username, password: this.state.password })
-      .then(res => {
+    this.toggleLoading();
+    await axios
+      .post("/auth/login", {
+        username: this.state.username,
+        password: this.state.password,
+      })
+      .then((res) => {
+        this.toggleLoading();
         this.props.LoginAction(res.data);
         history.push("/");
       })
       .catch(() => {
+        this.toggleLoading();
         // alert
-      })
+      });
+  }
+
+  toggleLoading() {
+    this.setState({
+      is_loading: !this.state.is_loading,
+    });
   }
 
   render() {
     return (
       <Row className="m-0 justify-content-center">
-        <Card className="rounded-0 mt-0 px-2">
-                      <CardBody>
-                        <Form onSubmit={e => e.preventDefault()}>
-                          <br/>
-                          <FormGroup className="form-label-group position-relative has-icon-left">
-                            <Input type="text" placeholder="Username" value={this.state.username} onChange={e => this.setState({ username: e.target.value })} />
-                            <div className="form-control-position">
-                              <Mail size={15} />
-                            </div>
-                            <Label>Username</Label>
-                          </FormGroup>
-                          <FormGroup className="form-label-group position-relative has-icon-left">
-                            <Input type="password" placeholder="Password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })} />
-                            <div className="form-control-position">
-                              <Lock size={15} />
-                            </div>
-                            <Label>Password</Label>
-                          </FormGroup>
-                          {/* <FormGroup className="d-flex justify-content-between align-items-center">
+        <Card className="rounded-1">
+          <CardBody>
+            <Form onSubmit={(e) => e.preventDefault()}>
+              <Col lg={12}>
+                <br></br>
+                <FormGroup className="form-label-group position-relative has-icon-left">
+                  <Input
+                    type="text"
+                    placeholder="Username"
+                    value={this.state.username}
+                    onChange={(e) =>
+                      this.setState({ username: e.target.value })
+                    }
+                  />
+                  <div className="form-control-position">
+                    <Mail size={15} />
+                  </div>
+                  <Label>Username</Label>
+                </FormGroup>
+                <FormGroup className="form-label-group position-relative has-icon-left">
+                  <Input
+                    type="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={(e) =>
+                      this.setState({ password: e.target.value })
+                    }
+                  />
+                  <div className="form-control-position">
+                    <Lock size={15} />
+                  </div>
+                  <Label>Password</Label>
+                </FormGroup>
+                {/* <FormGroup className="d-flex justify-content-between align-items-center">
                             <Checkbox color="primary" icon={<Check className="vx-icon" size={16} />} label="Remember me" />
                           </FormGroup> */}
-                          <div className="d-flex justify-content-center">
-                            <Button.Ripple type="relief" color="danger" onClick={() => this.login()} block={true}>
-                                Login 
-                            </Button.Ripple>
-                          </div>
-                        </Form>
-                      </CardBody>
-                </Card>
+                <Button.Ripple
+                  className="btn-block"
+                  type="relief"
+                  color="danger"
+                  onClick={() => this.login()}
+                  block={true}
+                >
+                  {this.state.is_loading ? (
+                    <Spinner color="white" size="sm" />
+                  ) : (
+                    "Login"
+                  )}
+                </Button.Ripple>
+              </Col>
+            </Form>
+          </CardBody>
+        </Card>
       </Row>
-    )
+    );
   }
 }
 
--- src/views/pages/Member.js
+++ src/views/pages/Member.js
@@ -1,41 +1,65 @@
 import React from "react";
 import { ChevronLeft, ChevronRight, Search } from "react-feather";
-import { Badge, Card, CardBody, Col, Container, Pagination, PaginationItem, PaginationLink, Row, Table, Input, FormGroup } from "reactstrap";
-import http from '../../http';
-import debounce from 'debounce-promise';
+import {
+  Badge,
+  Card,
+  CardBody,
+  Col,
+  Container,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Row,
+  Table,
+  Input,
+  FormGroup,
+  Spinner,
+} from "reactstrap";
+import http from "../../http";
+import debounce from "debounce-promise";
 
-const getMemberData = (page, perPage, search) => http.get('/member', { params: { per_page: perPage, page, name: search }});
-const getMemberDataDebounced = debounce(getMemberData, 400)
+const getMemberData = (page, perPage, search) =>
+  http.get("/member", { params: { per_page: perPage, page, name: search } });
+const getMemberDataDebounced = debounce(getMemberData, 400);
 
-class Member extends React.Component{
+class Member extends React.Component {
   state = {
     members: [],
     pagination: {
       page: 1,
       per_page: 7,
       total_data: 0,
-      total_page: 0
+      total_page: 0,
     },
-    search: ""
-  }
+    search: "",
+    is_loading: false,
+  };
 
   async loadData(page = 1, perPage = 7, debounced = false) {
     const retrieve = debounced ? getMemberDataDebounced : getMemberData;
+    this.toggleLoading(true);
     await retrieve(page, perPage, this.state.search)
-      .then(res => {
-        this.setState({ members: res.data.content, pagination: res.data.pagination })
+      .then((res) => {
+        this.toggleLoading(false);
+        this.setState({
+          members: res.data.content,
+          pagination: res.data.pagination,
+        });
       })
+      .catch(() => {
+        this.toggleLoading(false);
+      });
   }
 
   async next() {
     if (this.state.pagination.page < this.state.pagination.total_data) {
-      await this.loadData(this.state.pagination.page + 1)
+      await this.loadData(this.state.pagination.page + 1);
     }
   }
 
   async prev() {
     if (this.state.pagination.page > 1) {
-      await this.loadData(this.state.pagination.page - 1)
+      await this.loadData(this.state.pagination.page - 1);
     }
   }
 
@@ -48,41 +72,52 @@ class Member extends React.Component{
     await this.loadData();
   }
 
-  render(){
+  toggleLoading(val) {
+    this.setState({
+      is_loading: typeof val === "boolean" ? val : !this.state.is_loading,
+    });
+  }
 
+  render() {
     const member = this.state.members.map((i, index) => {
       return (
-      <tr key={index}>
-        <td>{ i.name }</td>
-        <td>{ i.nim }</td>
-        <td>{ i.class_of }</td>
-        <td>{ i.major_name }</td>
-        <td>
-          { i.is_alumni ? 
-          <Badge color="success" >Alumni</Badge> : 
-          <Badge color="info">Student</Badge> 
-          }
-        </td>
-      </tr>
-      )
-    })
+        <tr key={index}>
+          <td>{i.name}</td>
+          <td>{i.nim}</td>
+          <td>{i.class_of}</td>
+          <td>{i.major_name}</td>
+          <td>
+            {i.is_alumni ? (
+              <Badge color="success">Alumni</Badge>
+            ) : (
+              <Badge color="info">Student</Badge>
+            )}
+          </td>
+        </tr>
+      );
+    });
+
+    const spinner = (
+      <div className="text-center">
+        <br />
+        <Spinner color="danger" size="lg" />
+      </div>
+    );
 
     const table = (
       <Table responsive>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Nim</th>
-              <th>Class Of</th>
-              <th>Major</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            { member }
-          </tbody>
-        </Table>
-    )
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Nim</th>
+            <th>Class Of</th>
+            <th>Major</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        {this.state.is_loading ? spinner : <tbody>{member}</tbody>}
+      </Table>
+    );
 
     const paginationItems = [];
     const margin = 3;
@@ -107,54 +142,78 @@ class Member extends React.Component{
     }
 
     for (let i = start; i <= end; i++) {
-      paginationItems.push((
-        <PaginationItem active={i === this.state.pagination.page} key={`pag-${i}`}>
-          <PaginationLink onClick={() => {this.loadData(i)}}>{i}</PaginationLink>
+      paginationItems.push(
+        <PaginationItem
+          active={i === this.state.pagination.page}
+          key={`pag-${i}`}
+        >
+          <PaginationLink
+            onClick={() => {
+              this.loadData(i);
+            }}
+          >
+            {i}
+          </PaginationLink>
         </PaginationItem>
-      ));
+      );
     }
 
     const pagination = (
       <Pagination className="d-flex justify-content-end mt-2 pagination-danger">
         <PaginationItem>
-          <PaginationLink onClick={() => { this.prev() }} first>
+          <PaginationLink
+            onClick={() => {
+              this.prev();
+            }}
+            first
+          >
             <ChevronLeft />
           </PaginationLink>
         </PaginationItem>
-        { paginationItems }
+        {paginationItems}
         <PaginationItem>
-          <PaginationLink onClick={() => { this.next() }} last>
+          <PaginationLink
+            onClick={() => {
+              this.next();
+            }}
+            last
+          >
             <ChevronRight />
           </PaginationLink>
         </PaginationItem>
-    </Pagination>
-    )
+      </Pagination>
+    );
 
     const searchbar = (
       <FormGroup className="position-relative has-icon-left">
-      <Input type="text" value={this.state.search} placeholder="Search Member" onChange={(e) => this.handleSearchOnchange(e.target.value)} />
-      <div className="form-control-position">
-        <Search size={15} />
-      </div>
-    </FormGroup>
-    )
+        <Input
+          type="text"
+          value={this.state.search}
+          placeholder="Search Member"
+          onChange={(e) => this.handleSearchOnchange(e.target.value)}
+        />
+        <div className="form-control-position">
+          <Search size={15} />
+        </div>
+      </FormGroup>
+    );
 
     return (
       <Container>
         <Row>
           <Col>
             <Card>
-                <CardBody>
-                  { searchbar }
-                  { table }
-                  { this.state.members.length !== 0 ? pagination : null }
-                </CardBody>
+              <CardBody>
+                {searchbar}
+                {table}
+                {this.state.members.length !== 0 ? pagination : null}
+              </CardBody>
             </Card>
           </Col>
         </Row>
       </Container>
-    )
+    );
   }
 }
 
-export default Member
+export default Member;
